95b358ad55864844f71aad86923ab9570cd963e7
Robert Buj <robert.buj@gmail.com>
2025-10-27 11:49:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Install Required Libraries:
+
+```r
+# Totes les dependències estan llistades a install.R
+source("install.R")
+```
+
+```shell
+ brew install pandoc
+ ```
---
+++ install.R
@@ -0,0 +1,13 @@
+# Install required packages for R-cast course
+install.packages(c(
+  "languageserver", # for R language server support for VSCode
+  "readr", # for reading CSV files
+  "readxl", # for reading Excel files
+  "dplyr", # for data manipulation
+  "rmarkdown", # for dynamic report generation
+  "knitr", # for dynamic report generation
+  "ggplot2", # for data visualization
+  "corrplot", # for correlation plots
+  "kableExtra", # for enhanced table formatting
+  "VIM" # for visualization and imputation of missing values
+))
